92a05a026e2f7700ff81be7d80f05f0dd0318d0a
Daisy Tucker <daisyatucker@gmail.com>
2025-05-01 20:43:14 +0100

    Amend padding on timeline

--- src/components/DetailedServices.tsx
+++ src/components/DetailedServices.tsx
@@ -8,7 +8,7 @@ const DetailedServices: React.FC = () => {
     {
       title: "Brand Strategy",
       content: (
-        <motion.div 
+        <motion.div
           className="text-charcoal font-roboto"
           initial={{ opacity: 0, x: 20 }}
           whileInView={{ opacity: 1, x: 0 }}
@@ -19,11 +19,11 @@ const DetailedServices: React.FC = () => {
             We partner with our clients to develop their brand identity and shape their narrative.
           </p>
           <p className="text-charcoal text-sm md:text-base font-light mb-6 font-roboto">
-            Our team specialises in crafting data-driven brand strategies that build strong market identities and foster customer loyalty. 
+            Our team specialises in crafting data-driven brand strategies that build strong market identities and foster customer loyalty.
             Our informed approach helps brands create compelling narratives, advocating for their products and ensuring they are able to stand out from the noise.
           </p>
           <div className="mb-6">
-            <motion.div 
+            <motion.div
               className="flex gap-2 items-center text-charcoal font-roboto text-sm mb-2"
               initial={{ opacity: 0, x: 20 }}
               whileInView={{ opacity: 1, x: 0 }}
@@ -32,7 +32,7 @@ const DetailedServices: React.FC = () => {
             >
               — Brand positioning and messaging
             </motion.div>
-            <motion.div 
+            <motion.div
               className="flex gap-2 items-center text-charcoal font-roboto text-sm mb-2"
               initial={{ opacity: 0, x: 20 }}
               whileInView={{ opacity: 1, x: 0 }}
@@ -41,7 +41,7 @@ const DetailedServices: React.FC = () => {
             >
               — Market research and consumer insights
             </motion.div>
-            <motion.div 
+            <motion.div
               className="flex gap-2 items-center text-charcoal font-roboto text-sm"
               initial={{ opacity: 0, x: 20 }}
               whileInView={{ opacity: 1, x: 0 }}
@@ -57,7 +57,7 @@ const DetailedServices: React.FC = () => {
     {
       title: "Reputation Management",
       content: (
-        <motion.div 
+        <motion.div
           className="text-charcoal font-roboto"
           initial={{ opacity: 0, x: 20 }}
           whileInView={{ opacity: 1, x: 0 }}
@@ -68,11 +68,11 @@ const DetailedServices: React.FC = () => {
             We leverage our relationships with editors, influencers and key decision makers to build and protect our clients' reputations.
           </p>
           <p className="text-charcoal text-sm md:text-base font-light mb-6 font-roboto">
-            Our team's network sits across tier one national and international outlets in Europe and the US. 
+            Our team's network sits across tier one national and international outlets in Europe and the US.
             While our focus is positioning luxury consumer brands, our team's experience spans sectors including financial services, technology, fashion, and travel.
           </p>
           <div className="mb-6">
-            <motion.div 
+            <motion.div
               className="flex gap-2 items-center text-charcoal font-roboto text-sm mb-2"
               initial={{ opacity: 0, x: 20 }}
               whileInView={{ opacity: 1, x: 0 }}
@@ -81,7 +81,7 @@ const DetailedServices: React.FC = () => {
             >
               — Media relations
             </motion.div>
-            <motion.div 
+            <motion.div
               className="flex gap-2 items-center text-charcoal font-roboto text-sm mb-2"
               initial={{ opacity: 0, x: 20 }}
               whileInView={{ opacity: 1, x: 0 }}
@@ -90,7 +90,7 @@ const DetailedServices: React.FC = () => {
             >
               — Crisis management
             </motion.div>
-            <motion.div 
+            <motion.div
               className="flex gap-2 items-center text-charcoal font-roboto text-sm"
               initial={{ opacity: 0, x: 20 }}
               whileInView={{ opacity: 1, x: 0 }}
@@ -106,7 +106,7 @@ const DetailedServices: React.FC = () => {
     {
       title: "Content Marketing",
       content: (
-        <motion.div 
+        <motion.div
           className="text-charcoal font-roboto"
           initial={{ opacity: 0, x: 20 }}
           whileInView={{ opacity: 1, x: 0 }}
@@ -117,11 +117,11 @@ const DetailedServices: React.FC = () => {
             We help our clients to explain complex issues succinctly to establish trust and build loyalty.
           </p>
           <p className="text-charcoal text-sm md:text-base font-light mb-6 font-roboto">
-            Our team of strategists and copywriters have substantial experience writing for clients across fashion, sustainability, finance, and technology. 
+            Our team of strategists and copywriters have substantial experience writing for clients across fashion, sustainability, finance, and technology.
             As a result, they are well-versed in dissecting complex concepts and positioning them for a broad range of audiences, including high-net-worth individuals, government and regulatory bodies, and tier one media.
           </p>
           <div className="mb-6">
-            <motion.div 
+            <motion.div
               className="flex gap-2 items-center text-charcoal font-roboto text-sm mb-2"
               initial={{ opacity: 0, x: 20 }}
               whileInView={{ opacity: 1, x: 0 }}
@@ -130,7 +130,7 @@ const DetailedServices: React.FC = () => {
             >
               — Speechwriting
             </motion.div>
-            <motion.div 
+            <motion.div
               className="flex gap-2 items-center text-charcoal font-roboto text-sm mb-2"
               initial={{ opacity: 0, x: 20 }}
               whileInView={{ opacity: 1, x: 0 }}
@@ -139,7 +139,7 @@ const DetailedServices: React.FC = () => {
             >
               — Website copywriting
             </motion.div>
-            <motion.div 
+            <motion.div
               className="flex gap-2 items-center text-charcoal font-roboto text-sm"
               initial={{ opacity: 0, x: 20 }}
               whileInView={{ opacity: 1, x: 0 }}
