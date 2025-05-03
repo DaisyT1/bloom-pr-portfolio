e271a42c0f4ac27318cd5f795be533f7c1ea5d7d
Daisy Tucker <daisyatucker@gmail.com>
2025-05-03 19:49:08 +0100

    Amend timeline colour

--- src/components/ui/timeline.tsx
+++ src/components/ui/timeline.tsx
@@ -56,10 +56,10 @@ export const Timeline = ({
               <div className="h-10 absolute left-3 md:left-3 w-10 rounded-full bg-offwhite flex items-center justify-center">
                 <div className={`h-4 w-4 rounded-full ${activeIndex === index ? 'bg-gradient-to-t from-[#FFCAD4] via-[#BDE4A8] to-transparent' : 'bg-neutral-200 dark:bg-neutral-800 border border-neutral-300 dark:border-neutral-700'} p-2`} />
               </div>
-              <h3 
+              <h3
                 className={`hidden md:block text-xl md:pl-20 md:text-3xl font-light font-roboto uppercase tracking-wider transition-colors duration-300 ${
-                  activeIndex === index 
-                    ? 'text-[rgb(203,213,225)]' 
+                  activeIndex === index
+                    ? 'text-pink-dark'
                     : 'text-charcoal'
                 }`}
               >
@@ -68,10 +68,10 @@ export const Timeline = ({
             </div>
 
             <div className="relative pl-20 pr-4 md:pl-4 w-full">
-              <h3 
+              <h3
                 className={`md:hidden block text-2xl mb-4 text-left font-light font-roboto uppercase tracking-wider transition-colors duration-300 ${
-                  activeIndex === index 
-                    ? 'text-[rgb(203,213,225)]' 
+                  activeIndex === index
+                    ? 'text-pink-dark'
                     : 'text-charcoal'
                 }`}
               >
